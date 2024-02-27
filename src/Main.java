d98e14186ae0522fa7f53da3ba39b79c01fe2c6e
Youssef Mohamed Taman <119713458+youssefMohamedTaman@users.noreply.github.com>
2024-02-27 03:16:59 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+ public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> remainders = new ArrayList<>();
+        ArrayList<Integer> moduli = new ArrayList<>();
+        int size;
+        System.out.println("input the size");
+        size = scan.nextInt();
+        scan.nextLine();
+        for (int i = 0; i < size; i++) {
+            System.out.printf("input the #%d equation\n", i + 1);
+            String input = scan.nextLine();
+            StringBuilder ip = new StringBuilder(input);
+            for (int j = 0; j < ip.length(); j++) {
+                if (ip.charAt(j) == ' ') {
+                    ip.deleteCharAt(j);
+                    j--;
+                }
+            }
+            input = ip.toString();
+            StringBuilder num1 = new StringBuilder();
+            StringBuilder num2 = new StringBuilder();
+            for (int j = 0; j < input.length(); j++) {
+                if (input.charAt(j) == '=') {
+                    j++;
+                    while (input.charAt(j) != '(') {
+                        num1.append(input.charAt(j));
+                        j++;
+                    }
+                }
+                if (input.charAt(j) == 'd') {
+                    j++;
+                    while (input.charAt(j) != ')') {
+                        num2.append(input.charAt(j));
+                        j++ ;
+                    }
+                }
+            }
+            String number1 = num1.toString();
+            String number2 = num2.toString();
+            remainders.add(Integer.parseInt(number1));
+            moduli.add(Integer.parseInt(number2));
+
         }
+        ChineseRemainder chineseRemainder = new ChineseRemainder();
+        System.out.println(ChineseRemainder.chineseRemainder(remainders, moduli));
     }
+    
 }
